e3de450df147f0fcd62e74b5b5c84f69fdaaf06a
carlos zavala <cazcarlos@hotmail.com>
2020-05-10 22:16:05 -0500

    adding front-end with react and component Layout and auth to register

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Navbar from './component/layout/Navbar'
+import Landing from './component/layout/Landign'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Landing/>
+    </div>
+  );
+}
+
+export default App;
